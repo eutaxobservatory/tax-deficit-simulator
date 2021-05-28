2c4c00003cd0e663c563733a3b0c25f993090204
Paul-Emmanuel Chouc <pechouc@gmail.com>
2021-05-28 12:02:10 +0200

    Cleaning the repository

--- setup.py
+++ setup.py
@@ -5,12 +5,11 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
-setup(name='tax_deficit_simulator',
-      version="1.0",
-      description="Project Description",
-      packages=find_packages(),
-      test_suite='tests',
-      # include_package_data: to install data from MANIFEST.in
-      include_package_data=True,
-      # scripts=['scripts/tax-deficit-simulator-run'],
-      zip_safe=False)
+setup(
+    name='tax_deficit_simulator',
+    version="1.0",
+    description="Python package and online simulator accompanying the EU Tax Observatory report of June 2021.",
+    packages=find_packages(),
+    test_suite='tests',
+    zip_safe=False
+)
